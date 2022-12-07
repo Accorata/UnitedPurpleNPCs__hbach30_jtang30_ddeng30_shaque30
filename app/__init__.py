311fcc8eec20a8e09d574317dcbb630d80615329
Henry Bach <henrypbach@gmail.com>
2022-12-07 13:18:20 -0500

    py stuff

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,24 @@ import json
 app = Flask(__name__)
 # key = open("key_nasa.txt", "r").read()
 
+db_name = "discobandit.db"
+
+# db = sqlite3.connect(db_name)
+#     c = db.cursor()
+#     db.close()
+
 @app.route('/')
 def showindex():
     # url = request.urlopen(f"https://api.nasa.gov/planetary/apod?api_key={key}").read()
     # dict = json.loads(url)
     # , picture=dict['url'], explanation=dict['explanation'], head = dict['title']
-    return render_template('index.html')
+    
+    if 'username' in session :
+        return render_template('index.html')
+    else :
+        return render_template('login.html')
+
+
 
 if __name__ == "__main__":
     app.debug = True
