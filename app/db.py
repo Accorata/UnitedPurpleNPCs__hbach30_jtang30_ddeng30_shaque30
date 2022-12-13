242c3cf660abcc307efad7728633be3949a0747d
Henry Bach <henrypbach@gmail.com>
2022-12-13 13:50:02 -0500

    db rework

--- app/db.py
+++ app/db.py
@@ -8,9 +8,13 @@ time spent: 1 hrs
 
 import sqlite3
 
-DB_FILE = "p1_info.db"
+DB_FILE = "app/p1_info.db"
 db = sqlite3.connect(DB_FILE, check_same_thread = False)
 c = db.cursor()
 
-c.execute("""CREATE TABLE IF NOT EXISTS info(username TEXT UNIQUE, password TEXT, city TEXT, weather TEXT, temperature INTEGER,
+c.execute("""CREATE TABLE IF NOT EXISTS users(username TEXT UNIQUE, password TEXT)""")
+c.execute("""CREATE TABLE IF NOT EXISTS info(username TEXT UNIQUE, city TEXT, weather TEXT, temperature INTEGER,
  love_percent INTEGER)""")
+
+db.commit()
+db.close()
