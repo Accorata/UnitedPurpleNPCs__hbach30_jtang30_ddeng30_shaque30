cb3f3aaf2476c18ceec9d6206071968cfa7f8046
Henry Bach <henrypbach@gmail.com>
2022-12-15 14:04:10 -0500

    eoc

--- app/__init__.py
+++ app/__init__.py
@@ -29,17 +29,72 @@ db_name = "app/p1_info.db"
 def show_index():
     if 'username' not in session :
         return render_template('main.html')
-
     username = session['username']
+    (ip, date, time, week_day, weather) = get_user_info()
+    return render_template('index.html', username=username, ip=ip, date=date, time=time, weekday=week_day, weather=weather)#, month=month)
 
-    # Time from location (I'll move this to a seperate function soon)
 
-    ipstack_key = open("app/keys/ipstack_key.txt", "r").read()
+@app.route('/signup', methods = ["GET", "POST"]) # Sign up page
+def show_signup():
+    return render_template('signup.html')
 
+@app.route('/login', methods = ["GET", "POST"]) # Login page
+def show_login():
+    return render_template('login.html')
+
+@app.route('/new_account', methods = ["GET", "POST"])
+def create_user():
+    if flask_request.method == 'POST':
+        db = sqlite3.connect(db_name)
+        c = db.cursor()
+        user_list = c.execute("SELECT username from users;").fetchall()
+        #print(user_list)
+        if (flask_request.form['username'],) not in user_list: 
+            if flask_request.form['password'] == flask_request.form['password1']:
+                if len(flask_request.form['password']) > 0:
+                    username = flask_request.form['username']
+                    new_account = [username, flask_request.form['password']]
+                    c.execute("INSERT INTO users VALUES (?, ?)", new_account)
+                    db.commit()
+                    db.close()
+                    session['username'] = username
+                    return redirect(url_for('show_index'))
+                return render_template('signup.html', error = "Please enter a password.")
+            return render_template('signup.html', error = "Passwords do not match.")
+        return render_template('signup.html', error = "Username already exists.")
+    return "Error- not post"
+
+
+@app.route('/login_user', methods = ["POST"]) # Redirection to home page upon successful login
+def login():
+    if flask_request.method == 'POST':
+        db = sqlite3.connect(db_name)
+        c = db.cursor()
+        user_list = c.execute("SELECT username from users;").fetchall()
+        pw_list = c.execute("SELECT username, password from users;").fetchall()
+        username = flask_request.form['username']
+        pw = flask_request.form['password']
+        if (username,) not in user_list:
+            return render_template('login.html', error = "Username not found. Please create an account or try again.")
+        if (username, pw) not in pw_list:
+            return render_template('login.html', error = "Incorrect password for this username. Please try again.")
+        session['username'] = username
+        #print("check")
+        return redirect(url_for('show_index'))
+    return "Error- not post"
+
+
+@app.route('/logout', methods = ["POST"])
+def logout():
+    session.pop('username')
+    return redirect(url_for('show_index'))
+
+def get_user_info():
     url = f"https://api.ipify.org" # ***** We need to make an kb for this
     data = request.urlopen(url).read()
     ip = str(data)[2:-1]
-    # return ip
+
+    ipstack_key = open("app/keys/ipstack_key.txt", "r").read()
 
     url = f"http://api.ipstack.com/"+ip+"?access_key="+ipstack_key
 
@@ -56,15 +111,14 @@ def show_index():
     data = request.urlopen(url).read()
     weather_results = json.loads(data)['data'][0]
     #return weather_results;
-    weather_descption = weather_results['weather']['description']
+    weather_description = weather_results['weather']['description']
     location = weather_results['timezone']
+    #return weather_results
 
     url = f'https://worldtimeapi.org/api/timezone/'+location+'.json'
     data = request.urlopen(url).read()
     time_results = json.loads(data)
     time_data = time_results['datetime']
-
-    
 
     days_of_week = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday')
     week_day = days_of_week[int(time_results['day_of_week'])]
@@ -75,51 +129,7 @@ def show_index():
     day = time_data[8:10]
     year = time_data[0:4]
     time = time_data[11:16]
-
-    #return results
-    return render_template('index.html', username=username, ip=ip, date=month+" "+day+", "+year, time=time, weekday=week_day, weather=weather_descption)#, month=month)
-
-
-@app.route('/signup', methods = ["GET", "POST"]) # Sign up page
-def show_signup():
-    return render_template('signup.html')
-
-
-@app.route('/login', methods = ["GET", "POST"]) # Login page
-def show_login():
-    return render_template('login.html')
-
-
-@app.route('/new_account', methods = ["POST"])
-def create_account():
-    if flask_request.method == 'POST':
-        db = sqlite3.connect(db_name)
-        c = db.cursor()
-        user_list = c.execute("SELECT username from users;").fetchall()
-        #print(user_list)
-        if (flask_request.form['username'],) not in user_list: 
-            username = flask_request.form['username']
-            new_account = [username, flask_request.form['password']]
-            c.execute("INSERT INTO users VALUES (?, ?)", new_account)
-            db.commit()
-            db.close()
-            session['username'] = username
-            return redirect(url_for('show_index'))
-        return render_template('signup.html', error = "Username already exists")
-    return redirect(url_for('show_index'))
-
-
-@app.route('/login_user', methods = ["POST"]) # Redirection to home page upon successful login
-def login():
-    #login stuff
-    return redirect(url_for('show_index'))
-
-
-@app.route('/logout', methods = ["POST"])
-def logout():
-    session.pop('username')
-    return redirect(url_for('show_index'))
-
+    return (ip, month+" "+day+", "+year, time, week_day, weather_description)
 
 def get_ip():
     return flask_request.environ.get('HTTP_X_REAL_IP', flask_request.remote_addr)
@@ -127,6 +137,6 @@ def get_ip():
 if __name__ == "__main__":
     app.debug = True
     app.run()
-    
+
 def find_similar_results():
     return 100
