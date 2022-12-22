6d3fa87ec15d093bebbeb9a3bc7049d18b8a8967
Shafiul Haque <shaque30@stuy.edu>
2022-12-21 20:51:11 -0500

    toggle pw done

---
+++ app/static/js/visibility.js
@@ -0,0 +1,16 @@
+function showpw() {
+    var x = document.getElementById("pw");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+}
+function showpw2() {
+    var x = document.getElementById("pw2");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+}
